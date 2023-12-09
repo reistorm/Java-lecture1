228dbfbf45d03372ea7d87217cdfef8f2800b518
Элиза Рысаева <103875177+reistorm@users.noreply.github.com>
2023-12-09 23:58:36 +0500

    lib.java

--- lib.java
+++ lib.java
@@ -1,5 +1,12 @@
-public class lib {
-    static void sayHi(){
-        System.out.println("hi");
-    }
+// public class lib {
+//     static void sayHi(){
+//         System.out.println("hi");
+//     }
+// }
+
+class Sample{
+	public static void main(String args[]){
+		System.out.println("Welcome to GeeksForGeeks");
+	}
 }
+
